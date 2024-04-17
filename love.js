e45189cb456bbab0f638da7cb4c8304e6a9f9a2d
Grace Grumo <90876119+grcgrm@users.noreply.github.com>
2024-04-17 14:15:25 -0400

    new

--- love.js
+++ love.js
@@ -1,73 +1,22 @@
-// Array of possible content
-var contentArray = ["iloveyou", "imissyou", "want hug", "want kiss", "cuddle"];
+const carousel = document.querySelector(".carousel"),
+firstImg = carousel.querySelectorAll("img")[0],
+arrowIcons = document.querySelectorAll(".wrapper i");
 
-// Global record of all previously selected content
-var previousContent = [];
+let isDragStart = false, isDragging = false, prevPageX, prevScrollLeft, positionDiff;
 
-// Function to shuffle an array
-function shuffleArray(array) {
-    for (var i = array.length - 1; i > 0; i--) {
-        var j = Math.floor(Math.random() * (i + 1));
-        var temp = array[i];
-        array[i] = array[j];
-        array[j] = temp;
-    }
-    return array;
+const showHideIcons = () => {
+    // showing and hiding prev/next icon according to carousel scroll left value
+    let scrollWidth = carousel.scrollWidth - carousel.clientWidth; // getting max scrollable width
+    arrowIcons[0].style.display = carousel.scrollLeft == 0 ? "none" : "block";
+    arrowIcons[1].style.display = carousel.scrollLeft == scrollWidth ? "none" : "block";
 }
 
-// Function to generate random content
-function getRandomContent() {
-    // Shuffle the contentArray
-    var shuffledArray = shuffleArray(contentArray.slice());
-    
-    // Iterate through the shuffled array and select the first unique content
-    for (var i = 0; i < shuffledArray.length; i++) {
-        var newContent = shuffledArray[i];
-        
-        // Check if the newly selected content has already been used
-        // If it has, continue to the next content
-        if (previousContent.includes(newContent)) {
-            continue;
-        } else {
-            // Update the record of previously selected content
-            previousContent.push(newContent);
-            return newContent;
-        }
-    }
-    
-    // If all content has been used, reset the record of previously selected content
-    previousContent = [];
-    
-    // Recursively call getRandomContent to select content from the shuffled array
-    return getRandomContent();
-}
-
-// Apply random content to the pseudo-element for all elements with the class 'tz-gallery .lightbox'
-var elements = document.querySelectorAll('.tz-gallery .lightbox');
-elements.forEach(function(element) {
-    element.style.setProperty('--content', '"' + getRandomContent() + '"');
+arrowIcons.forEach(icon => {
+    icon.addEventListener("click", () => {
+        let firstImgWidth = firstImg.clientWidth + 14; // getting first img width & adding 14 margin value
+        // if clicked icon is left, reduce width value from the carousel scroll left else add to it
+        carousel.scrollLeft += icon.id == "left" ? -firstImgWidth : firstImgWidth;
+        setTimeout(() => showHideIcons(), 60); // calling showHideIcons after 60ms
+    });
 });
 
- // Get all accordion headers
- var accHeaders = document.querySelectorAll('.accordion-header');
-
- // Loop through all accordion headers
- accHeaders.forEach(function(accHeader) {
-   // Add click event listener to each accordion header
-   accHeader.addEventListener('click', function() {
-     // Check if the clicked accordion is already active
-     var isActive = this.classList.contains('active');
-
-     // Close all accordion items
-     accHeaders.forEach(function(header) {
-       header.classList.remove('active');
-       header.nextElementSibling.style.display = 'none';
-     });
-
-     // If the clicked accordion was not active, open it
-     if (!isActive) {
-       this.classList.toggle('active');
-       this.nextElementSibling.style.display = 'block';
-     }
-   });
- });
